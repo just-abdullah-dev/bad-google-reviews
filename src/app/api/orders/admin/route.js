3af7996dfd505772d77e32fda2a5051adea88ecc
Abdullah <abdulhaseeb9274@gmail.com>
2024-09-25 21:42:54 +0500

    added OPTIONS in a/api/orders/admin for cors setup, also tried another tactics as well

--- src/app/api/orders/admin/route.js
+++ src/app/api/orders/admin/route.js
@@ -5,6 +5,32 @@ import { ID, Query } from "node-appwrite";
 const db_id = process.env.APPWRITE_DB_ID;
 const collection_id = process.env.APPWRITE_ORDERS_C_ID;
 
+const allowedOrigins = [
+  "https://bad-google-reviews.vercel.app",
+  "http://localhost:3000",
+  "http://127.0.0.1:3000",
+];
+
+export async function OPTIONS(req) {
+  const origin = req.headers.get("origin");
+
+  if (allowedOrigins.includes(origin)) {
+    return NextResponse.json(
+      {},
+      {
+        status: 204,
+        headers: {
+          "Access-Control-Allow-Origin": origin,
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+      }
+    );
+  }
+
+  return NextResponse.json({ message: "CORS not allowed" }, { status: 403 });
+}
+
 export async function GET(req) {
   try {
     const response = await ssDatabase.listDocuments(db_id, collection_id);
@@ -30,48 +56,66 @@ export async function GET(req) {
 
 export async function PUT(req) {
   try {
-    const body = await req.json();
-    const { orderId, status, deletedNoOfReviews } = body;
-
-    const orders = await ssDatabase.listDocuments(db_id, collection_id, [
-      Query.equal("$id", orderId),
-    ]);
-    const order = orders?.documents[0];
-    if (!order) {
+    // console.log("DB_ID:", db_id, "COLLECTION_ID:", collection_id);
+    if (req.headers.get("content-type") !== "application/json") {
       return NextResponse.json(
-        {
-          success: false,
-          message: "No order was found.",
-        },
-        { status: 404 }
+        { success: false, message: "Invalid content type" },
+        { status: 400 }
       );
     }
-    let data = {
-      status,
-    };
-    if (status === "fulfilled" || status === "partially-fulfilled") {
-      data = {
+    const origin = req.headers.get("origin");
+
+    if (allowedOrigins.includes(origin)) {
+      const body = await req.json();
+      const { orderId, status, deletedNoOfReviews } = body;
+
+      const orders = await ssDatabase.listDocuments(db_id, collection_id, [
+        Query.equal("$id", orderId),
+      ]);
+      const order = orders?.documents[0];
+      if (!order) {
+        return NextResponse.json(
+          {
+            success: false,
+            message: "No order was found.",
+          },
+          { status: 404 }
+        );
+      }
+      let data = {
         status,
-        finalCost:
-          (process.env.NEXT_PUBLIC_PRICE_PER_REVIEW *
-          Number(deletedNoOfReviews)),
-        deletedNoOfReviews: Number(deletedNoOfReviews),
       };
+      if (status === "fulfilled" || status === "partially-fulfilled") {
+        data = {
+          status,
+          finalCost:
+            process.env.NEXT_PUBLIC_PRICE_PER_REVIEW *
+            Number(deletedNoOfReviews),
+          deletedNoOfReviews: Number(deletedNoOfReviews),
+        };
+      }
+      const response = await ssDatabase.updateDocument(
+        db_id,
+        collection_id,
+        order?.$id,
+        data
+      );
+      // console.log(response);
+      return NextResponse.json(
+        {
+          success: true,
+          message: "Order status has been updated.",
+        },
+        {
+          status: 200,
+          headers: {
+            "Access-Control-Allow-Origin": origin,
+          },
+        }
+      );
     }
-    const response = await ssDatabase.updateDocument(
-      db_id,
-      collection_id,
-      order?.$id,
-      data
-    );
-    console.log(response);
-    return NextResponse.json(
-      {
-        success: true,
-        message: "Order status has been updated.",
-      },
-      { status: 200 }
-    );
+
+    return NextResponse.json({ message: "CORS not allowed" }, { status: 403 });
   } catch (error) {
     console.error("Error updating order document:", error);
     return NextResponse.json(
